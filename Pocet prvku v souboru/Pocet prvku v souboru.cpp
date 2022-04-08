e46b8a361e87234a0785a37505c0e9a9b0e1ce25
Petr Habel <petrhabel04@gmail.com>
2022-04-08 14:52:05 +0200

    Přidejte soubory projektu.

---
+++ Pocet prvku v souboru/Pocet prvku v souboru.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+
+int main() {
+	int pocet[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	int cisla[10];
+	int znak = 0;
+	int cislo = 0;
+
+
+	FILE* soubor = fopen("soubor.txt", "r");
+
+
+	if (soubor == NULL) {
+		printf("Soubor nelze otevrit");
+		return 1;
+	}
+
+
+	while (!feof(soubor)) {
+		fscanf(soubor, "%d", &cisla[znak]);
+		znak++;
+	}
+	for (int i = 0; i < 10; i++) {
+		cislo = cisla[i];
+		pocet[cislo]++;
+	}
+	for (int i = 0; i < 10; i++) printf("Cislo %d je v souboru %dx\n", i, pocet[i]);
+
+
+	return(0);
+}
